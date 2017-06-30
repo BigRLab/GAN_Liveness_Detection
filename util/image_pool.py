59a9d9daa10420c20eb3c6e83208a70077e8560b
yunfan0621 <yunfan@umich.edu>
2017-06-30 14:37:30 +0800

    re-structured files to be ignored

--- util/image_pool.py
+++ util/image_pool.py
@@ -3,6 +3,8 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
+import pdb
+
 class ImagePool():
     def __init__(self, pool_size):
         self.pool_size = pool_size
@@ -11,18 +13,22 @@ class ImagePool():
             self.images = []
 
     def query(self, images):
+        # images: torch.Variable of size [batch_size, channel * 2, w, h]
+
         if self.pool_size == 0:
             return images
         
         return_images = []
-        for image in images.data:
-            image = torch.unsqueeze(image, 0) # insert the batch dimension
+        for image in images.data: # traverse data in batch dimension
+            image = torch.unsqueeze(image, 0)
+
             if self.num_imgs < self.pool_size:
                 self.num_imgs = self.num_imgs + 1
                 self.images.append(image)
                 return_images.append(image)
             else:
                 p = random.uniform(0, 1)
+                # randomly substitute
                 if p > 0.5:
                     random_id = random.randint(0, self.pool_size-1)
                     tmp = self.images[random_id].clone()
--- util/util.py
+++ util/util.py
@@ -13,3 +13,15 @@ def tensor2im(image_tensor, imtype=np.uint8):
     image_numpy = image_tensor[0].cpu().float().numpy()
     image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0 # the default range of image is (-1, 1)
     return image_numpy.astype(imtype)
+
+def mkdirs(paths):
+    if isinstance(paths, list) and not isinstance(paths, str):
+        for path in paths:
+            mkdir(path)
+    else:
+        mkdir(paths)
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+    	os.makedirs(path)
