c65b61688985d5835ddeaae92a719f7fe411fc04
yunfan0621 <yunfan@umich.edu>
2017-06-30 15:49:02 +0800

    finished the code for data manipulation

---
+++ data/data_loader.py
@@ -0,0 +1,7 @@
+def CreateDataLoader(opt):
+    from data.custom_dataset_data_loader import CustomDatasetDataLoader
+    
+    data_loader = CustomDatasetDataLoader()
+    data_loader.initialize(opt)
+    
+    return data_loader
